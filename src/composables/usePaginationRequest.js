11bd8523296d35de77945eb7bd4dcf4902c6f977
ChrisLey <201357337@qq.com>
2023-06-13 23:14:53 +0800

    minor: 单词修正uniqueField

--- src/composables/usePaginationRequest.js
+++ src/composables/usePaginationRequest.js
@@ -16,7 +16,7 @@ import { getDeepProperty } from '~/utils'
  * @param { string } keywordKey 搜索关键字在请求参数中的名称，默认为keyword
  * @param { object } extraParams 除了分页和搜索关键字以外的其他参数, 应当是一个Ref或ComputedRef, 或者返回一个对象的函数以兼容选项式API
  * @param { array } defaultValue 适用于多选的编辑场景下的回显数据，多选时为对象数组
- * @param { string } uniqueFiled 适用于多选的编辑场景下，回显数据去重的字段，默认为id
+ * @param { string } uniqueField 适用于多选的编辑场景下，回显数据去重的字段，默认为id
  * @param { (msg: string) => void } warning 异常提示方法
  * @param { (...args) => void } requestCallback 请求成功且在options处理完成后的回调
  * @param { boolean } replace 旧数据的处理方式: true - 替换; false - 并入。默认false
@@ -32,7 +32,7 @@ function usePaginationRequest(
     keywordKey = 'keyword',
     extraParams = {},
     defaultValue = [],
-    uniqueFiled = 'id',
+    uniqueField = 'id',
     warning = () => {},
     requestCallback,
     replace = false,
@@ -89,7 +89,7 @@ function usePaginationRequest(
             let currentData = getDeepProperty(res, dataKey)
             if (!keyword.value && merge && !keywordChanged) {
                 currentData = currentData
-                    .filter(item => preData.value.every(preItem => preItem[uniqueFiled] !== item[uniqueFiled]))
+                    .filter(item => preData.value.every(preItem => preItem[uniqueField] !== item[uniqueField]))
             }
 
             count.value = getDeepProperty(res, countKey)
